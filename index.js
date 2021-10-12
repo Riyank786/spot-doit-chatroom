3e3fd4b94423aa732d7a06ced46a220be285452c
riyank786 <riyank7086@gmail.com>
2021-10-12 21:16:41 +0530

    update 1.1.16

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ var express = require('express');
 var app = express();
 
 var server = app.listen(process.env.PORT || 8000);
+app.use(express.static(__dirname));
 
 // server = process.env.PORT || 8000;
 
 app.get('/', function (req, res) {
-    res.sendFile('index.html', {root: __dirname })
+    res.sendFile('index.html')
   })
 
 const io = require("socket.io")(server, {
