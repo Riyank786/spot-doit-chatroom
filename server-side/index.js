0c2de1135e5bb3845e1f077a5ffa8c515ec4b178
riyank786 <riyank7086@gmail.com>
2021-10-12 21:00:35 +0530

    update 1.1.10

--- server-side/index.js
+++ server-side/index.js
@@ -6,7 +6,7 @@ var app = express();
 
 var server = app.listen(process.env.PORT || 8000);
 
-server = process.env.PORT || 8000;
+// server = process.env.PORT || 8000;
 
 app.get('/', function (req, res) {
     res.send('../index.html')
