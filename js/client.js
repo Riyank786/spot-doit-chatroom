ec41ea6ea264bd620e0b47e7e67728db02c8b56f
riyank786 <riyank7086@gmail.com>
2021-10-12 20:39:02 +0530

    update 1.1.6

---
+++ js/client.js
@@ -0,0 +1,60 @@
+
+const socket = io();
+
+const form = document.getElementById('sendForm');
+const messageInp = document.getElementById('messageInp');
+const messageContainer = document.querySelector('.message-container');
+
+
+var audio = new Audio('assests/sounds/message.mp3');
+
+const appendMessage = (message, position) => {
+
+    const messageWrapper = document.createElement('div');
+    messageWrapper.classList.add('message-wrapper');
+    messageWrapper.classList.add(position);
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    if(position != 'center'){
+        messageElement.classList.add('message');
+    }
+    messageWrapper.append(messageElement);
+    messageContainer.append(messageWrapper);
+
+    if(position == 'left' || position == 'center'){
+        audio.play();
+    }
+    
+
+}
+
+
+const userName = prompt("Enter your good name to join");
+socket.emit('new-user-joined',  userName);
+
+
+socket.on('userJoined', (name, id) => {
+    appendMessage(`${name} joined the chat`, 'center');
+});
+
+socket.on('receive', data => {
+    appendMessage(`${data.name}: ${data.message}`,'left')
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+})
+
+socket.on('left', userName =>{
+    appendMessage(`${userName} left the chat`, 'center');
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+})
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+    const message = messageInp.value;
+    appendMessage(`${message}`, 'right');
+    messageInp.value = '';
+    socket.emit('send', message);
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+
+})
+
---
+++ server-side/index.js
@@ -0,0 +1,37 @@
+
+// const FIREBASE_TOKEN = '1//0gMvCOvrSwdcLCgYIARAAGBASNwF-L9IrfriUjKj-xFwq30DcruouJyrjMTqSriNRpl4GsAuuNwZ6besjLssElAft-Vqylcn0hsA';
+
+// var express = require('express');
+// var app = express();
+
+// var server = app.listen(process.env.PORT || 8000);
+
+server = process.env.PORT || 8000;
+
+console.log('checking........');
+// const io = require("socket.io")(server, {
+//     cors: {
+//       origin: "*",
+//       credentials: true
+//     }
+//   });
+
+
+// const users = {};
+
+// io.on('connection', socket => {
+
+
+//     socket.on('new-user-joined', name => {
+//         users[socket.id] = name;
+//         socket.broadcast.emit('userJoined', name, users[socket.id]);
+//     })
+
+//     socket.on('send', message => {
+//         socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+//     });
+
+//     socket.on('disconnect', () => {
+//         socket.broadcast.emit('left', users[socket.id]);
+//     })
+// })
